6475d960e3466a7d06c53b4086bb335eb895b0c3
diozenio <d.dionisiobsl@gmail.com>
2025-07-04 20:15:04 -0300

    feat: Add shadcn config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Inter, Lora } from "next/font/google";
 import "./globals.css";
 
 import { AppProvider } from "@/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const loraSerif = Lora({
+  variable: "--font-lora-serif",
   subsets: ["latin"],
 });
 
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.variable} ${geistMono.variable} ${loraSerif.variable} dark`}
       >
         <AppProvider>{children}</AppProvider>
       </body>
