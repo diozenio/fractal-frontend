e3063045f59bd7b2bd3806988fd08ebb71b5eb0b
diozenio <d.dionisiobsl@gmail.com>
2025-07-14 13:24:54 -0300

    feat(auth): Implement login page and form components

---
+++ src/ui/components/auth/login-form.tsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/ui/primitives/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/ui/primitives/card";
+import { Input } from "@/ui/primitives/input";
+import { Label } from "@/ui/primitives/label";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Entrar na sua conta</CardTitle>
+          <CardDescription>
+            Insira seu e-mail abaixo para acessar sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="seu@email.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu a senha?
+                  </a>
+                </div>
+                <Input id="password" type="password" name="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Entrar
+                </Button>
+                <Button variant="outline" className="w-full" type="button">
+                  Entrar com o Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Ainda não tem uma conta?{" "}
+              <a href="/auth/signup" className="underline underline-offset-4">
+                Cadastre-se
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
