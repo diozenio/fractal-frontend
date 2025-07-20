275dd83f1a3cdf23a67b6d10433141cb0b572f6d
diozenio <d.dionisiobsl@gmail.com>
2025-07-20 04:26:05 -0300

    fix(auth): Update AuthService to use AuthAPI instead of AuthMock

--- src/container/index.ts
+++ src/container/index.ts
@@ -2,7 +2,7 @@ import AuthService from "@/core/services/AuthService";
 import AuthAPI from "@/infra/auth/AuthAPI";
 import AuthMock from "@/infra/auth/AuthMock";
 
-const AuthServiceInstance = new AuthService(new AuthMock());
+const AuthServiceInstance = new AuthService(new AuthAPI());
 
 export const services = {
   AuthService: AuthServiceInstance,
